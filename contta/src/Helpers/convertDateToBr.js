1c4ca35ab65b785fc0ee3355c847231a146da156
cegj <cegasparjr@gmail.com>
2022-11-19 01:53:52 -0300

    Fix date converter helper bug

--- contta/src/Helpers/convertDateToBr.js
+++ contta/src/Helpers/convertDateToBr.js
@@ -1,6 +1,10 @@
 const convertDateToBr = (dateString) => {
-  const date = new Date(dateString)
-  return date.toLocaleString().split(" ")[0]
+
+  //Receive date on ISO format YYYY-MM-DD and convert to BR format DD/MM/AAAA
+
+  const arr = dateString.replace(/-/g, '/').split('/')
+  const converted = arr[2] + '/' + arr[1] + '/' + arr[0];
+  return converted
 }
 
 export default convertDateToBr
