01be6c0d3be3d8d008072030d2c1c9c1d5d4a2cf
cegj <cegasparjr@gmail.com>
2022-11-24 12:35:56 -0300

    Create status filter and clear all filters on statement

--- contta/src/Components/Statement/Statement.js
+++ contta/src/Components/Statement/Statement.js
@@ -16,12 +16,13 @@ const Statement = () => {
   const [typeFilter, setTypeFilter] = React.useState(null)
   const [categoryFilter, setCategoryFilter] = React.useState(null)
   const [accountFilter, setAccountFilter] = React.useState(null)
+  const [statusFilter, setStatusFilter] = React.useState(null)
   const [hasFilter, setHasFilter] = React.useState(false)
 
   React.useEffect(() => {
-    if(typeFilter || categoryFilter || accountFilter) setHasFilter(true)
+    if(typeFilter || categoryFilter || accountFilter || statusFilter) setHasFilter(true)
     else setHasFilter(false)
-  }, [typeFilter, categoryFilter, accountFilter, hasFilter])
+  }, [typeFilter, categoryFilter, accountFilter, statusFilter, hasFilter])
 
   return (
     <>
@@ -33,6 +34,9 @@ const Statement = () => {
       setCategoryFilter={setCategoryFilter}
       accountFilter={accountFilter}
       setAccountFilter={setAccountFilter}
+      statusFilter={statusFilter}
+      setStatusFilter={setStatusFilter}
+      hasFilter={hasFilter}
     />
     <div className={styles.statementContainer}>
       {(transactions && transactions.length > 0) ?
@@ -41,6 +45,7 @@ const Statement = () => {
           if (typeFilter && (transaction.type !== typeFilter.value)) return null
           if (categoryFilter && (transaction.category_id !== categoryFilter.value)) return null
           if (accountFilter && (transaction.account_id !== accountFilter.value)) return null
+          if (statusFilter && (transaction.preview !== statusFilter.value)) return null
           else return <StatementItem key={transaction.id} {...transaction} />
         } else return <StatementItem key={transaction.id} {...transaction} /> 
       })
--- contta/src/Components/Statement/StatementFilterBar.js
+++ contta/src/Components/Statement/StatementFilterBar.js
@@ -4,15 +4,25 @@ import styles from './StatementFilterBar.module.css'
 import Select from 'react-select'
 import selectStylesFilterBar from '../../options/selectStyles-FilterBar'
 import {ReactComponent as FilterIcon} from '../../assets/icons/filter_icon.svg'
+import {ReactComponent as FilterOffIcon} from '../../assets/icons/filter_off_icon.svg'
 
-const StatementFilterBar = ({typeFilter, setTypeFilter, categoryFilter, setCategoryFilter, accountFilter, setAccountFilter}) => {
+const StatementFilterBar = ({typeFilter, setTypeFilter, categoryFilter, setCategoryFilter, accountFilter, setAccountFilter, statusFilter, setStatusFilter, hasFilter}) => {
 
   const {typeOptions, categoryOptions, accountOptions} = React.useContext(TransactionsContext)
 
+  const statusOptions = [{label: 'Previsto', value: 1}, {label: 'Consolidado', value: 0}]
+
+  function clearAllFilters(){
+    setTypeFilter(null)
+    setCategoryFilter(null)
+    setAccountFilter(null)
+    setStatusFilter(null)
+  }
+
   return (
     <div className={styles.statementFilterBar}>
-      <div className={styles.filterIcon}>
-        <FilterIcon />
+      <div data-tip={hasFilter ? 'Limpar filtros' : null} className={`${styles.filterIcon} ${hasFilter ? styles.hasFilter : ''}`} onClick={hasFilter ? clearAllFilters : null}>
+        {!hasFilter ? <FilterIcon /> : <FilterOffIcon />} 
       </div>
       <div className={styles.filterControl}>
         <label htmlFor="typeFilter">Tipo</label>
@@ -51,6 +61,19 @@ const StatementFilterBar = ({typeFilter, setTypeFilter, categoryFilter, setCateg
           value={accountFilter}
           onChange={setAccountFilter}
           options={accountOptions}
+        />
+      </div>
+      <div className={styles.filterControl}>
+        <label htmlFor="statusFilter">Situação</label>
+        <Select
+          name="statusFilter"
+          id="statusFilter"
+          placeholder="Selecione..."
+          isClearable={true}
+          styles={selectStylesFilterBar}
+          value={statusFilter}
+          onChange={setStatusFilter}
+          options={statusOptions}
         />
       </div>
     </div>
